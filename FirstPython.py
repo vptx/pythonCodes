69f4fd49f7af16a94a061a981a6382124af9aa33
vptx <yazan.1999ok@hotmail.com>
2022-01-17 18:25:49 +0200

    See my first

---
+++ FirstPython.py
@@ -0,0 +1,50 @@
+#my first program in python 
+print(288775897 )
+print ("hello yazan \n")
+#multi programs
+print('''hello world 
+this is multy line''')
+num = 22
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
+
+def compute_lcm(x, y):
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1 = 54
+num2 = 24
+
+print("The L.C.M. is", compute_lcm(num1, num2))
